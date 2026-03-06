8779224d5156a3a45ce114b022badb7ef404d53e
hyunwoo0976 <rhw3884@gmail.com>
2026-03-06 15:46:55 +0900

    python

---
+++ python/Project1_basic/ex1.py
@@ -0,0 +1,7 @@
+print("(5+5)*4/2=",(5+5)*4/2)       #20.0으로 출력
+print("python"+"program")           #두 문장이 붙어서 출력
+print("python","program")           #두 문장이 한 칸 띄어서 출력
+print(10+10)                        #20출력
+print(10,10)                        #10 10 출력
+print("파이썬"*5)                    #파이썬 문장 5번연달아출력
+print("python"+str(5))              #python5로 출력 str은 문자열 함수임
